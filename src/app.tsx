ae4665bece1d591c859f70d1ee8fd8cc0998aad2
123 <123>
2024-11-21 11:17:31 +0800

    下面是用户管理前端界面

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,14 @@ import {AvatarDropdown, AvatarName, Footer, Question} from '@/components';
 import {LinkOutlined} from '@ant-design/icons';
 import type {Settings as LayoutSettings} from '@ant-design/pro-components';
 import {SettingDrawer} from '@ant-design/pro-components';
-import type {RequestConfig, RunTimeLayoutConfig} from '@umijs/max';
 import {history, Link} from '@umijs/max';
 import defaultSettings from '../config/defaultSettings';
-import {errorConfig} from './requestErrorConfig';
 import {currentUser as queryCurrentUser} from './services/ant-design-pro/api';
 
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
+//无需用户登录态的页面
+const NO_NEED_LOGIN_WHITE_LIST = ['/user/register', loginPath];
 
 /**
  * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
@@ -22,27 +22,25 @@ export async function getInitialState(): Promise<{
 }> {
   const fetchUserInfo = async () => {
     try {
-      const msg = await queryCurrentUser({
-        skipErrorHandler: true,
-      });
-      return msg.data;
+      return await queryCurrentUser();
     } catch (error) {
       //history.push(loginPath);
     }
     return undefined;
   };
   // 如果不是登录页面，执行
-  const {location} = history;
-  if (![loginPath, '/user/register', '/user/register-result'].includes(location.pathname)) {
-    const currentUser = await fetchUserInfo();
+  // 如果是登录页面，不执行
+  //如果是注册页面，不需要登录，在该白名单中，执行
+  if (NO_NEED_LOGIN_WHITE_LIST.includes(location.pathname)) {
     return {
       fetchUserInfo,
-      currentUser,
       settings: defaultSettings as Partial<LayoutSettings>,
     };
   }
+  const currentUser = await fetchUserInfo();
   return {
     fetchUserInfo,
+    currentUser,
     settings: defaultSettings as Partial<LayoutSettings>,
   };
 }
@@ -62,13 +60,12 @@ export const layout: ({initialState, setInitialState}: {
       },
     },
     waterMarkProps: {
-      content: initialState?.currentUser?.name,
+      content: initialState?.currentUser?.username,
     },
     footerRender: () => <Footer/>,
     onPageChange: () => {
       const {location} = history;
-      const whiteList = ['/user/register', loginPath];
-      if (whiteList.includes(location.pathname)) {
+      if (NO_NEED_LOGIN_WHITE_LIST.includes(location.pathname)) {
         return;
       }
 
--- src/services/ant-design-pro/typings.d.ts
+++ src/services/ant-design-pro/typings.d.ts
@@ -2,25 +2,18 @@
 /* eslint-disable */
 
 declare namespace API {
+  //封装的类型 这里直接粘贴我们user数据表的条目
   type CurrentUser = {
-    name?: string;
-    avatar?: string;
-    userid?: string;
-    email?: string;
-    signature?: string;
-    title?: string;
-    group?: string;
-    tags?: { key?: string; label?: string }[];
-    notifyCount?: number;
-    unreadCount?: number;
-    country?: string;
-    access?: string;
-    geographic?: {
-      province?: { label?: string; key?: string };
-      city?: { label?: string; key?: string };
-    };
-    address?: string;
+    id?: number;
+    username?: string;
+    userAccount: string;
+    avatarUrl: string;
+    gender?: number;
     phone?: string;
+    email?: string;
+    userStatus?: number;
+    userRole?: number;
+    createTime?: Date;
   };
 
   type LoginResult = {
